65adeff9bf86e225bc258b0d7955b5d837cda0b3
joeybrey <jbrey15@gmail.com>
2022-02-07 08:18:56 -0500

    Create Handling_ErrorsApp.swift

---
+++ Handling Errors/Handling Errors/Handling Errors/Handling_ErrorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Handling_ErrorsApp.swift
+//  Handling Errors
+//
+//  Created by Joseph Breymeier on 1/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Handling_ErrorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
