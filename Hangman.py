61ab7cab7374088d954731cb6f7b1f46ab798fc1
joeybrey <jbrey15@gmail.com>
2022-02-14 12:55:20 -0500

    commit on 'main'

--- Hangman.py
+++ Hangman.py
@@ -1,64 +1,50 @@
+#MAKE GAMEBOT WITH MAD LIBS AND NUMBER GUESSING GAME
+
 import random
-from tkinter import Y
 
 correct = []
 incorrect = []
-
-def none_incorrect():
-	print(" 0 ")
-	print("/|\ ")
-	print(" /\ ")
-
-def correct_guess():
-	correct.append(guess)
-	print("Good guess! {} is in the word.".format(guess))
-
-
-def incorrect_guess():
-	guesses_left = 6
-	guesses_left -= 1
-	print("Ooh sorry, it looks like {} is not in the word I'm thinking of. You have {} guesses left".format(str(guess), str(guesses_left)))
-	incorrect.append(guess)
-guesses_left = incorrect_guess()
-def make_guess():
-	if guess in correct:
-		print("You've already guessed that letter, silly!")
-	elif guess in incorrect:
-		print("You've already guessed that letter, silly!")
+guesses_left = 6
+words = ["jessica", "joey", "eva", "laney", "david", "marissa"]
+word = random.choice(words)
+good_synonyms = ["fantastic", "great", "wonderful", "marvelous", "exceptional"]
+def correct_guess(guess):
+	if guess == word:
+		print("Yes! You guessed the word! {} job.".format(random.choice(good_synonyms)))
+def make_guess(guess):
+	if guess in incorrect:
+		print("You've already guessed {}, silly!".format(guess))
+		print("Here are the letters you got correct: {}".format(correct))
+		print("Here are the letters you got incorrect: {}".format(incorrect))
+	elif guess in correct:
+		print("You've already guessed {}, silly!".format(guess))
+		print("Here are the letters you got correct: {}".format(correct))
+		print("Here are the letters you got incorrect: {}".format(incorrect))
 	elif guess in word:
-		correct_guess()
+		print("Yes! There is a(n) {} in the word!".format(guess))
+		correct.append(guess)
+		print("Here are the letters you got correct: {}".format(correct))
+		print("Here are the letters you got incorrect: {}".format(incorrect))
 	else:
-		incorrect_guess()
-
-def post_guess():
-	print("Here are the letters you got correct: {}".format(correct))
-	print("Here are the letters you got incorrect: {}".format(incorrect))
-	print("Make another guess!")
-
-print("\nWelcome to hangman! I'm going to think of a word and I'll let you guess letters until you compose the word. You have 6 incorrect guesses. Let's play!")
+		incorrect.append(guess)
+		global guesses_left
+		if guesses_left > 0:
+			guesses_left -= 1
+			print("Ooh sorry, it looks like {} is not in the word I'm thinking of. You have {} guesses left.".format(guess, str(guesses_left)))
+			print("Here are the letters you got correct: {}".format(correct))
+			print("Here are the letters you got incorrect: {}".format(incorrect))
+			return guesses_left
+print("\nWelcome to hangman! I'm going to think of a word and I'll let you guess letters until you compose the word. You have 6 incorrect guesses. Let's play!")	
 ready_or_not = input("Ready? [Y/N] \n")
 if ready_or_not.lower() == "y":
 	while guesses_left > 0:
-		words = ["jessica", "joey", "eva", "laney", "david", "marissa"]
-		word = random.choice(words)
-		guess = input("Guess a letter. \n")
-		make_guess()
-		post_guess()
-		guess = input("Guess a letter. \n")
-		make_guess()
-		post_guess()
-		guess = input("Guess a letter. \n")
-		make_guess()
-		post_guess()
-		guess = input("Guess a letter. \n")
-		make_guess()
-		post_guess()
-		guess = input("Guess a letter. \n")
-		make_guess()
-		post_guess()
-		guess = input("Guess a letter. \n")
-	if guesses_left == 0:
-		print("It looks like you're all out of guesses! Good game. Please play again.")
+		guess = input("Guess a letter. \n\n")
+		if guess == word:
+			correct_guess(guess)
+		else:
+			make_guess(guess)
+	else:
+		print("You are all out of guesses! Thank you for playing.")
 elif ready_or_not.lower() == "n":
 	print("Okay, come back whenever you are ready to play :) \n")	
 else:
